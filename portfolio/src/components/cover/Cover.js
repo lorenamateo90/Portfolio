817568673c4be0c869af8f9732c59b08dddddc5b
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-09-29 14:59:43 +0200

    add new video-cover

--- portfolio/src/components/cover/Cover.js
+++ portfolio/src/components/cover/Cover.js
@@ -4,7 +4,7 @@ import coverVideo from "../../media/coverVideo.mp4"
 const Cover = () => {
     return (
         <div className="cover">
-            <video className="video" src={coverVideo} autoPlay loop muted />
+            <video className="video" src={coverVideo} autoPlay muted />
             <h1 className="cover__name">Lorena Mateo</h1>
             <p className="cover__job">Desarrolladora Frontend</p>
         </div>
