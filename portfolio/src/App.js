1138b1d8d40fd4b4c270bcb82e4a198d760ca953
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-10-04 18:09:52 +0200

    add navbar links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,24 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Cover from "./components/cover/Cover";
+import Navbar from './components/navbar/Navbar';
+
 function App() {
+  const [scrollHeight, setScrollHeight] = useState (0);
+  
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+  };
+  
+  useEffect(()=> (
+    window.addEventListener("scroll", handleScroll)
+  ), [scrollHeight])
+
   return (
     <div className="App">
-     <Cover/>
+    <Navbar isScrolling={scrollHeight}/>
+    <Cover/>
     </div>
   );
 }
---
+++ portfolio/src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Navbar.scss";
+
+const Navbar = ({isScrolling}) => {
+    return (
+        <nav className={`navbar ${isScrolling >20 ? "scrolling" : null}`}>
+            <div>
+                <ul className="navbar__list">
+                    <li className="navbar__list--item"><a className="links" href="#aboutMe">Sobre mi
+                        </a>
+                    </li>
+                    <li className="navbar__list--item"><a className="links" href="#skills">Habilidades</a></li>
+                    <li className="navbar__list--item"><a className="links" href="#projects">Proyectos</a></li>
+                    <li className="navbar__list--item"><a className="links" href="#contact">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
