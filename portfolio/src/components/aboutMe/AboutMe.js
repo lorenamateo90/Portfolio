3e564eaef596e62ef7f7297589d4a451f4cfaf99
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-10-25 19:26:36 +0200

    add button cv

--- portfolio/src/components/aboutMe/AboutMe.js
+++ portfolio/src/components/aboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./AboutMe.scss";
-import photo from "../../media/photo.jpg";
+import photo from "../../images/photo.jpg";
+import cv from "../../images/cv.pdf";
 
 const AboutMe = () => {
     return (
@@ -8,11 +9,14 @@ const AboutMe = () => {
             <div className="description">
             <h1 className="description__title">¡Hola! Soy Lorena</h1>
             <p>Soy politóloga de formación y desarrolladora por convicción. Llegué al sector tecnológico en busca de nuevos retos con los que poder comprometerme, aportar lo mejor de mí y poder crecer profesionalmente. Gracias a mi bagaje laboral tan dispar he desarrollado muchas competencias que pueden encajar perfectamente en este mundo, como la gestión de equipos, la impartición de formación o la resolución de problemas, entre otras tantas. Actualmente estoy en búsqueda de mi primer trabajo como desarrolladora frontend en Madrid o en remoto.</p>
+            <div className="button">
+            <a href={cv}  className="button__cv" target="_blank" rel="noopener noreferrer" download>Descargar CV</a>
+            </div>
             </div>
             <div className="left">
                 <img className="photo" src={photo} alt="foto desarrolladora" />
             </div>
-            
+          
         </section>
     )
 }
