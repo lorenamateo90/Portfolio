b01db63d924f8b12f21b044f0f3ac4c15260d1fb
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-11-12 19:21:22 +0100

    add the project section

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import './Projects.scss';
+import React from "react";
+import { projects } from "../../data";
+import "./Projects.scss";
 
 const Projects = () => {
   return (
-    <div id="projects"></div>
-  )
+    <section id="projects">
+
+      
+        
+        {/* <div className="project__container">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="">
+              <div className="image">
+                <img
+                  alt="gallery"
+                  className="image"
+                  src={project.image}
+                />
+                <div className="description" >
+                  <p className="">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+       */}
+          <div className="projects__container">
+          {projects.map((project) => (
+          
+            <a className="cards"
+               href={project.link} 
+               target="_blank" 
+               rel="noreferrer"
+               >
+                  <img
+                  alt="imagenes de los proyectos"
+                  className="cards__images"
+                  src={project.image}
+                />
+            </a>  
+        
+          ))}
+        </div>
+          <div className="github">
+          <a className="github__button" href="https://github.com/lorenamateo90"
+           target="_blank" 
+           rel="noreferrer"
+           
+           ><i className="fab fa-github"></i>Ver más proyectos</a>
+        
+          </div> 
+         
+   
+      
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ portfolio/src/data.js
@@ -0,0 +1,49 @@
+import baila from "./images/Baila.png";
+import blackJack from "./images/Blackjack.jpeg";
+import buscador from "./images/Buscador.jpg";
+import DiseñaTarjeta from "./images/DiseñaTarjeta.png";
+import Weather from "./images/Weather.png";
+import RickMorty from "./images/RickMorty.jpg";
+
+
+
+export const projects = [
+
+    {
+    // description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: blackJack,
+      link: "https://github.com/lorenamateo90/blackjack",
+    },
+    {
+     
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: baila,
+      link: "https://github.com/lorenamateo90/Web-BAILA",
+    },
+    {
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: buscador,
+      link: "https://github.com/lorenamateo90/BuscadorDeSeries",
+    },
+    {
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: DiseñaTarjeta,
+      link: "https://github.com/lorenamateo90/AwesomeProfileCardsReact",
+    },
+    {
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: Weather,
+      link: "",
+    },
+    {
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: RickMorty,
+      link: "https://github.com/lorenamateo90/RickandMorty",
+    },
+  ];
