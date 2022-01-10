fe968dcc73f86faed8b6fe60d115010faa1aa44a
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2022-01-10 20:30:30 +0100

    navbar responsive

--- portfolio/src/components/navbar/Navbar.js
+++ portfolio/src/components/navbar/Navbar.js
@@ -1,28 +1,48 @@
 
 import React from 'react';
 import "./Navbar.scss";
+import { useState, useEffect } from 'react';
 
 const Navbar = ({isScrolling}) => {
-
-
+  
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
     
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    };
+
+    useEffect(() => {
+
+        const changeWidth = () => {
+          setScreenWidth(window.innerWidth);
+        }
+    
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    
+    
+      }, [])
+
     return (
         <nav className={`navbar ${isScrolling >20 ? "scrolling" : null}`}>
+             
             <div>
-            <button className="navbar__open" aria-label="Abrir menú">
-                <i class="fas fa-bars"></i>
-                </button>
+            {(toggleMenu || screenWidth > 1200) && (
                 <ul className="navbar__list">
-                <button className="navbar__close" aria-label="Cerat menú">
-                <i class="fas fa-times"></i>
-                </button>
-                    <li className="navbar__list--item"><a className="links" href="#aboutMe">Sobre mi</a></li>
-                    <li className="navbar__list--item"><a className="links" href="#skills">Habilidades</a></li>
-                    <li className="navbar__list--item"><a className="links" href="#projects">Proyectos</a></li>
-                    <li className="navbar__list--item"><a className="links" href="#contact">Contacto</a></li>
+                    <li className="navbar__list--item" onClick={toggleNav}><a className="links" href="#aboutMe">Sobre mi</a></li>
+                    <li className="navbar__list--item" onClick={toggleNav}><a className="links" href="#skills">Habilidades</a></li>
+                    <li className="navbar__list--item" onClick={toggleNav}><a className="links" href="#projects">Proyectos</a></li>
+                    <li className="navbar__list--item" onClick={toggleNav}><a className="links" href="#contact">Contacto</a></li>
                 </ul>
-                
-            </div>
+          
+             )}
+               </div>
+                <button onClick={toggleNav} className="btn">
+                <i class="fas fa-bars"></i></button> 
         </nav>
     )
 }
